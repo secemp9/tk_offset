0aaaeb97adcad02387084182fd50ee52fcef4f1d
Nordine Lotfi <71677764+secemp9@users.noreply.github.com>
2022-11-16 16:34:17 +0100

    corrected more grammar

--- example.py
+++ example.py
@@ -10,7 +10,7 @@ box_text.pack()
 
 txt = """hello world"""
 
-len_txt = len(txt) # get total lenght of the text content. Can be replaced by `os.path.getsize` or other alternative for files
+len_txt = len(txt) # get the total length of the text content. Can be replaced by `os.path.getsize` or other alternatives for files
 
 main_text.insert(tk.INSERT, txt)
 
@@ -24,7 +24,7 @@ def offset():
 
     try:
         to_do = inputValue.split("-")
-        if len(to_do) == 1: # if lenght is 1, it probably is a single offset for a single byte/char
+        if len(to_do) == 1: # if length is 1, it probably is a single offset for a single byte/char
             to_do.append(to_do[0])
             # second = to_do[0]
         if int(to_do[0]) > int(to_do[1]): # This is to support reverse range offset, so 11-2 -> 2-11, etc
