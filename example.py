22e7ee480b4ec22c170eb86aae9f44dd438eb91e
Nordine Lotfi <71677764+secemp9@users.noreply.github.com>
2022-11-16 16:27:12 +0100

    Creating example.py
    Just an example demonstrating the feasibility of offsets support. See the readme for the TODO

---
+++ example.py
@@ -0,0 +1,53 @@
+import tkinter as tk
+
+
+root = tk.Tk()
+
+main_text = tk.Text(root)
+
+box_text = tk.Text(root, height=1, width=10)
+box_text.pack()
+
+txt = """hello world"""
+
+len_txt = len(txt) # get total lenght of the text content. Can be replace by `os.path.getsize` or other alternative for files
+
+main_text.insert(tk.INSERT, txt)
+
+def offset():
+    inputValue = box_text.get("1.0", "end-1c") # get the input of the text widget without newline (since it's added by default)
+
+    # focusing the other text widget, deleting and re-insert the original text so that the selection/tag is updated (no need to move the mouse to the other widget in this example)
+    main_text.focus()
+    main_text.delete("1.0", tk.END)
+    main_text.insert(tk.INSERT, txt)
+
+    try:
+        to_do = inputValue.split("-")
+        if len(to_do) == 1: # if lenght is 1, it probably is a single offset for a single byte/char
+            to_do.append(to_do[0])
+            # second = to_do[0]
+        if int(to_do[0]) > int(to_do[1]): # This is to support reverse range offset, so 11-2 -> 2-11, etc
+            first = int(to_do[1]) - 1
+            first = str(first).split("-")[-1:][0]
+
+            second = (int(to_do[0]) - len_txt) - 1
+            second = str(second).split("-")[-1:][0]
+        else: # use the offset range normally
+            first = int(to_do[0]) - 1
+            first = str(first).split("-")[-1:][0]
+
+            second = (int(to_do[1]) - len_txt) - 1
+            second = str(second).split("-")[-1:][0]
+
+        print(first, second)
+        main_text.tag_add("sel", '1.0 + {}c'.format(first), 'end - {}c'.format(second))
+    except Exception as test: # not good practice, but hopefully should be fine for this small example
+        pass
+
+buttonCommit = tk.Button(root, text="use offset",
+                    command=lambda: offset())
+buttonCommit.pack()
+main_text.pack(fill="both", expand=1)
+main_text.pack_propagate(0)
+root.mainloop()
